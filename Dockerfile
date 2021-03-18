005e90246e85c3f9270fc8d321d911974881265b
PauloAlves8039 <pj38alves@gmail.com>
2021-03-18 14:35:57 -0300

    Definindo conexão com o MySQL e configurações com o Docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM microsoft/dotnet:2.1-aspnetcore-runtime
-LABEL version="1.0.1" description="Aplicação ASP .NET Core MVC"
+LABEL version="1.0.2" description="Aplicação ASP .NET Core MVC"
 COPY dist /app
 WORKDIR /app
 EXPOSE 80/tcp
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using mvc1.Models;
@@ -32,6 +33,14 @@ namespace mvc1
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            var host = Configuration["DBHOST"] ?? "localhost";
+            var port = Configuration["DBPORT"] ?? "3306";
+            var password = Configuration["DBPASSWORD"] ?? "*******";
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql($"server={host};userid=root;pwd={password};"
+                    + $"port={port};database=produtosdb"));
+
             services.AddTransient<IRepository, TesteRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -52,6 +61,8 @@ namespace mvc1
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             // app.UseCookiePolicy();
+
+            PopulaDb.IncluiDadosDB(app);
 
             app.UseMvc(routes =>
             {
